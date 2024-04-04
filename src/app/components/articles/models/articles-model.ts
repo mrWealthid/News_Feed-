22dc85c7d2ca28d48a459c32da81937b10d43c4f
wealthiduwe <wealthiduwe@gmail.com>
2024-04-04 01:06:36 -0400

    Add: Added article model for interface

---
+++ src/app/components/articles/models/articles-model.ts
@@ -0,0 +1,12 @@
+export interface IArticles {
+  title: string;
+  description: string;
+  author: string;
+  publishedAt: string;
+  url: string;
+  urlToImage: string;
+  source: {
+    id: string;
+    name: string;
+  };
+}
