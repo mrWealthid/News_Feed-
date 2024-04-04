83a2dfbb7a59a6394e0d11d074eb8090bd4e63f1
wealthiduwe <wealthiduwe@gmail.com>
2024-04-04 14:30:11 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { IArticles } from './components/articles/models/articles-model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'newsFeed';
+ 
 }
