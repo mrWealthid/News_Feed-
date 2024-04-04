2a0f1d1ebbbd441a90c2c1e09f79a40114032f3e
wealthiduwe <wealthiduwe@gmail.com>
2024-04-04 01:03:38 -0400

    Add: Added a new article service to fetch articles

---
+++ src/app/services/articles.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { IArticles } from '../components/articles/models/articles-model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArticlesService {
+  apiUrl =
+    'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=36524571b10a41338dd9ba92462b5818';
+
+  private http = inject(HttpClient);
+  constructor() {}
+
+  getArticles(): Observable<IArticles[]> {
+    return this.http.get<IArticles[]>(this.apiUrl).pipe(
+      map((data: any) => {
+        return data.articles;
+      })
+    );
+  }
+}
