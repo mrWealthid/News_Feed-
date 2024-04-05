de47b3bf4447c4636d4a0bf58194bc71f61ba575
wealthiduwe <wealthiduwe@gmail.com>
2024-04-05 03:40:13 -0400

    Add: Added Skeleton loader to show loading state

---
+++ src/app/components/article/article-loader.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'article-loader',
+  templateUrl: './article-loader.component.html',
+})
+export class ArticleLoaderComponent {}
