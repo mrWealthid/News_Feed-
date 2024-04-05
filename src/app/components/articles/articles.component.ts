a0215de39371dac679cd2266dfba4241bea49f40
wealthiduwe <wealthiduwe@gmail.com>
2024-04-05 03:41:53 -0400

    Optimize: Optimized article state to ensure observable is subscribed to once

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ArticlesService } from 'src/app/services/articles.service';
-import { IArticles } from './models/articles-model';
+import { IArticles, IArticlesState } from './models/articles-model';
 
 @Component({
   selector: 'app-articles',
@@ -9,47 +9,33 @@ import { IArticles } from './models/articles-model';
   styleUrls: ['./articles.component.scss'],
 })
 export class ArticlesComponent implements OnInit {
-  title: any = 'News Feed';
+  title: 'News Feed' | 'Bookmarks' = 'News Feed';
 
   private articleService = inject(ArticlesService);
-  articles$: Observable<IArticles[]> = this.articleService.getArticles();
-  bookmarks: IArticles[] = [];
-
-  error$: Observable<boolean> = this.articles$.pipe(
-    map(() => false), // no error, return false
-    catchError((error) => {
-      // On error, log it and switch to an Observable that emits true
-      console.error('Error fetching articles:', error);
-      return of(true);
-    })
-  );
+  articles$: Observable<IArticlesState> = this.articleService.getArticles();
+  bookmarks: IArticles[];
 
   ngOnInit(): void {
     this.bookmarks = this.articleService.getBookmarks() || [];
-    console.log(this.bookmarks);
-    // this.articles = this.articleService.getArticles();
   }
 
-  toggleView(text: string) {
+  toggleView(text: 'News Feed' | 'Bookmarks') {
     this.title = text;
-    // this.bookmarks = this.articleService.getBookmarks();
   }
 
   addBoomark(article: IArticles) {
     this.bookmarks.push(article);
     this.updateLocalStorage();
-    // localStorage.setItem('bookmarks', JSON.stringify(this.bookmarks));
   }
-
-  updateLocalStorage() {
-    localStorage.setItem('bookmarks', JSON.stringify(this.bookmarks));
-  }
-
   removeBookmark(articleId: string) {
     this.bookmarks = this.bookmarks.filter(
       (bookmark: IArticles) => bookmark.url !== articleId
     );
     this.updateLocalStorage();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('bookmarks', JSON.stringify(this.bookmarks));
   }
 
   isBookmarked(article: IArticles) {
--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -1,25 +1,45 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
-import { IArticles } from '../components/articles/models/articles-model';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+import {
+  IArticles,
+  IArticlesState,
+} from '../components/articles/models/articles-model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticlesService {
-  apiUrl =
-    'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=36524571b10a41338dd9ba92462b5818';
+  apiUrl = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${environment.API_KEY}`;
 
   private http = inject(HttpClient);
   constructor() {}
 
-  getArticles(): Observable<IArticles[]> {
-    return this.http.get<IArticles[]>(this.apiUrl).pipe(
-      map((data: any) => {
-        return data.articles;
-      })
+  getArticles(): Observable<IArticlesState> {
+    return this.http.get(this.apiUrl).pipe(
+      map((data: any) => ({
+        loading: false,
+        data: data.articles,
+        error: null,
+      })),
+      startWith({ loading: true, data: [], error: null }),
+      catchError(({ error }) =>
+        of({
+          loading: false,
+          error: error.message,
+          data: [],
+        })
+      )
     );
   }
+
+  //   .pipe(
+  //     map((data: any) => {
+  //       return data.articles;
+  //     })
+  //   );
+  // }
 
   getBookmarks() {
     return JSON.parse(localStorage.getItem('bookmarks')!);
